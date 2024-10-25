03c45f05adc36a7ab0e222c500adc40e199f435d
janek_szyndlarewicz <jan.szyndlarewicz@gmail.com>
2024-10-25 23:34:23 +0200

    Docker file configuration

--- CLOCKEDIN_Backend/settings.py
+++ CLOCKEDIN_Backend/settings.py
@@ -18,8 +18,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-yb@$f@%-ih*!8lczv4_wg%!8v5v)lhjf72jp(xy93*gieqg-a&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -168,22 +168,22 @@ WSGI_APPLICATION = 'CLOCKEDIN_Backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': os.getenv('DB_ENGINE'),
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': 'localhost',
+#         'PORT': '3307',
+#     }
+# }
 
 
 # Password validation
